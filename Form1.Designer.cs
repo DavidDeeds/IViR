e36eba5fcbd53cd907f702f0746b38914178c03a
DavidDeeds <mistereadie@gmail.com>
2020-11-29 18:59:46 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace IViR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GetButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CamURLtextBox = new System.Windows.Forms.TextBox();
+            this.UsernametextBox = new System.Windows.Forms.TextBox();
+            this.PasswordtextBox = new System.Windows.Forms.TextBox();
+            this.LogOutputtextBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GetButton
+            // 
+            this.GetButton.Location = new System.Drawing.Point(260, 68);
+            this.GetButton.Margin = new System.Windows.Forms.Padding(2);
+            this.GetButton.Name = "GetButton";
+            this.GetButton.Size = new System.Drawing.Size(56, 19);
+            this.GetButton.TabIndex = 0;
+            this.GetButton.Text = "GET";
+            this.GetButton.UseVisualStyleBackColor = true;
+            this.GetButton.Click += new System.EventHandler(this.GetButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 13);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Camera URL:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 72);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password:";
+            // 
+            // CamURLtextBox
+            // 
+            this.CamURLtextBox.Location = new System.Drawing.Point(85, 11);
+            this.CamURLtextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.CamURLtextBox.Name = "CamURLtextBox";
+            this.CamURLtextBox.Size = new System.Drawing.Size(233, 20);
+            this.CamURLtextBox.TabIndex = 4;
+            this.CamURLtextBox.Text = "http://192.168.1.85/cgi-bin/snapshot.cgi?";
+            // 
+            // UsernametextBox
+            // 
+            this.UsernametextBox.Location = new System.Drawing.Point(85, 40);
+            this.UsernametextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.UsernametextBox.Name = "UsernametextBox";
+            this.UsernametextBox.Size = new System.Drawing.Size(76, 20);
+            this.UsernametextBox.TabIndex = 5;
+            this.UsernametextBox.Text = "Admin";
+            // 
+            // PasswordtextBox
+            // 
+            this.PasswordtextBox.Location = new System.Drawing.Point(85, 69);
+            this.PasswordtextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.PasswordtextBox.Name = "PasswordtextBox";
+            this.PasswordtextBox.PasswordChar = '*';
+            this.PasswordtextBox.Size = new System.Drawing.Size(76, 20);
+            this.PasswordtextBox.TabIndex = 6;
+            // 
+            // LogOutputtextBox1
+            // 
+            this.LogOutputtextBox1.Location = new System.Drawing.Point(360, 28);
+            this.LogOutputtextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.LogOutputtextBox1.Multiline = true;
+            this.LogOutputtextBox1.Name = "LogOutputtextBox1";
+            this.LogOutputtextBox1.ReadOnly = true;
+            this.LogOutputtextBox1.Size = new System.Drawing.Size(139, 58);
+            this.LogOutputtextBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(360, 11);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Log output:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 586);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.LogOutputtextBox1);
+            this.Controls.Add(this.PasswordtextBox);
+            this.Controls.Add(this.UsernametextBox);
+            this.Controls.Add(this.CamURLtextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GetButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IViR";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GetButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox CamURLtextBox;
+        private System.Windows.Forms.TextBox UsernametextBox;
+        private System.Windows.Forms.TextBox PasswordtextBox;
+        private System.Windows.Forms.TextBox LogOutputtextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace IViR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LogOutputtextBox1.Text = "Ready.\r\n";
+        }
+
+        private void GetButton_Click(object sender, EventArgs e)
+        {
+            LogOutputtextBox1.Clear();
+            LogOutputtextBox1.AppendText("Requesting..\r\n");
+            LoadPageWithDigestAuthentication(CamURLtextBox.Text, UsernametextBox.Text, PasswordtextBox.Text);
+            LogOutputtextBox1.AppendText("Finished Request.");
+        }
+
+        private void LoadPageWithDigestAuthentication(string url, string username, string password)
+        {
+            Uri myUri = new Uri(url);
+            WebRequest myWebRequest = HttpWebRequest.Create(myUri);
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)myWebRequest;
+            NetworkCredential myNetworkCredential = new NetworkCredential(username, password);
+            CredentialCache myCredentialCache = new CredentialCache();
+            myCredentialCache.Add(myUri, "Digest", myNetworkCredential);
+
+            myHttpWebRequest.PreAuthenticate = true;
+            myHttpWebRequest.Credentials = myCredentialCache;
+
+            LogOutputtextBox1.AppendText("Querying camera..\r\n");
+            WebResponse myWebResponse = myWebRequest.GetResponse();
+            LogOutputtextBox1.AppendText("Got response..\r\n");
+
+            Image img = Image.FromStream(myWebResponse.GetResponseStream());
+            LogOutputtextBox1.AppendText("Saving image..\r\n");
+
+            img.Save(Application.StartupPath + @"\" + DateTime.Now.ToString("ddMMyyyy-HHmmss") + ".png");
+            LogOutputtextBox1.AppendText("Image saved..\r\n");
+
+            pictureBox1.Image = img;
+            LogOutputtextBox1.AppendText("Picturebox image loaded..\r\n");
+
+            myWebResponse.Close();
+            LogOutputtextBox1.AppendText("Close request stream..\r\n");
+
+        }
+
+    }
+}
